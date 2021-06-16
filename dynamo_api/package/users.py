af005ccc6dda873f62becff2ebead0ccdebf3842
EC2 Default User <ec2-user@ip-172-31-15-119.us-east-2.compute.internal>
2021-06-16 02:25:16 +0000

    dynamoDb

--- dynamo_api/package/users.py
+++ dynamo_api/package/users.py
@@ -2,13 +2,17 @@ import json
 import boto3
 import os
 
-database = os.environ["MY_DATABASE"]
+users_table = os.environ["MY_DATABASE"]
+dynamodb=boto3.resource('dynamodb')
+table=dynamodb.Table(users_table)
 
 def getUser(event, context):
     print(json.dumps({"running": True}))
-    print(json.dumps({
-        "database": database,
-    }))
+    response=table.get_item(
+        Key={
+            'pk': 'user',
+            'sk': 'age'
+        })
     return {
         'statusCode': 200,
       'body': json.dumps('Hello from Lambda!')
@@ -16,9 +20,15 @@ def getUser(event, context):
     
 def putUser(event, context):
     print(json.dumps({"running": True}))
-    print(json.dumps({
-        "database": database,
-    }))
+    
+    table.put_item(
+        Item={
+            'pk':'user',
+            'sk':'age',
+            'name': 'Mikaela',
+            'last_name': 'Garcia',
+        })
+  
     return {
         'statusCode': 200,
       'body': json.dumps('Hello from Lambda!')
